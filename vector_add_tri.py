aa7b2b4639610ce9b0342ed7272eaf683c350e2d
fmirkowski <mirkowskifranciszek@gmail.com>
2025-07-14 08:20:37 +0200

    initial - matmul from scratch, triton experiments

---
+++ vector_add_tri.py
@@ -0,0 +1,13 @@
+"""
+You will learn about:
+
+The basic programming model of Triton.
+
+The triton.jit decorator, which is used to define Triton kernels.
+
+The best practices for validating and benchmarking your custom ops against native reference implementations.
+
+"""
+
+
+
